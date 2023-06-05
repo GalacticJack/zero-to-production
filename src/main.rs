6cc8b66406a3a315913c5d9e2e980d2965ed1157
Jack Burton <jackson.burton@feddata.com>
2023-06-05 15:10:16 -0400

    Migrated from env_logger to tracing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 use std::net::TcpListener;
 
-use env_logger::Env;
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero_to_production::configuration::get_configuration;
 use zero_to_production::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect all `log`'s events to our sbuscriber
+    LogTracer::init().expect("Failed to set logger.");
+    // Setup and set default tracing subscriber
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("infor"));
+    let formatting_layer = BunyanFormattingLayer::new("zero_to_production".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber.");
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
