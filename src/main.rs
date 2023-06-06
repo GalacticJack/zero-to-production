5bad9616d45753e894157066b95f12053dff51db
Jack Burton <jackson.burton@feddata.com>
2023-06-06 10:04:44 -0400

    Create telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
 use std::net::TcpListener;
-
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero_to_production::configuration::get_configuration;
 use zero_to_production::startup::run;
+use zero_to_production::telemetry::{get_subscriber, init_subscriber}
+
+
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Redirect all `log`'s events to our sbuscriber
-    LogTracer::init().expect("Failed to set logger.");
-    // Setup and set default tracing subscriber
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("infor"));
-    let formatting_layer = BunyanFormattingLayer::new("zero_to_production".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber.");
+    let subscriber = get_subscriber("zero_to_production".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration");
